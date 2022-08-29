b00bb05a4d5ac7c3d357e8d4def60a7e113555b8
Gabriel Interiano <gabecyber4353@gmail.com>
2022-08-28 20:13:24 -0400

    commit

--- ACME-System/src/ver1/HelloWorld.java
+++ ACME-System/src/ver1/HelloWorld.java
@@ -7,5 +7,9 @@ public class HelloWorld {
 System.out.println("Hello from Eclipse");
 System.out.println("Gabe is working locally on his branch");
 	}
+	public static void heyNow() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
+	}
 
 }
